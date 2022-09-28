23e8b84a41a8c86178929216481989fd76f8bfb9
Đồng Thái Dương <93709370+ShowMakerCode@users.noreply.github.com>
2022-09-28 21:33:18 +0700

    Update ControllerCheckDemo.java

--- src/main/java/com/example/democonnectmysql/ControllerCheckDemo.java
+++ src/main/java/com/example/democonnectmysql/ControllerCheckDemo.java
@@ -14,15 +14,7 @@ public class ControllerCheckDemo {
     HibernateConection hibernateConection;
     @GetMapping("index")
     public String indexGet(Model model){
-//        Person person1 = new Person(1,"Duong","IT1",1);
-//        Person person2 = new Person(2,"Duong2","IT2",2);
-//        Person person3 = new Person(3,"Duong2","IT3",10);
-//        ArrayList<Person> listPersons = new ArrayList<>();
-//        listPersons.add(person1);
-//        listPersons.add(person2);
-//        listPersons.add(person3);
         List<Person> listPersonx = hibernateConection.getAllIlike();
-
         model.addAttribute("listPersons",listPersonx);
         return "index";
     }
